7142b62b0d332434c1f2ceb9cd0c9ae596f9105f
shagun9999 <110155117+shagun9999@users.noreply.github.com>
2022-07-29 17:17:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 28 19:39:27 2022
+
+@author: kishorekumar
+"""
+
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('linear123.pkl','rb')) 
+
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+    
+    
+    '''
+    For rendering results on HTML GUI
+    '''
+    exp = float(request.args.get('exp'))
+    
+    prediction = model.predict([[exp]])
+    
+        
+    return render_template('index.html', prediction_text='Regression Model  has predicted Price  for given Sqft is : {}'.format(prediction))
+
+
+if __name__ == "__main__":
+  app.run(debug = True)
